23fbe2476161dcebd3b973f7fe3703ebcad021aa
Rahul Guarv <126690105+rahul428742@users.noreply.github.com>
2024-08-13 23:57:35 +0530

    sidebar

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function openSidebar() {
+    document.getElementById("sidebar").style.width = "250px";
+    document.body.classList.add("");
+}
+
+function closeSidebar(){
+    document.getElementById("sidebar").style.width = "0";
+}
