10207496c252120b6d5ae976d219778a2d2ce71c
Rahul Guarv <126690105+rahul428742@users.noreply.github.com>
2024-08-15 00:32:56 +0530

    dark-light done

--- js/script.js
+++ js/script.js
@@ -8,3 +8,17 @@ function closeSidebar() {
     document.getElementById("sidebar").style.width = "0";
     document.body.classList.remove("sidebar-open");
 }
+
+function toggleDarkLight(mode) {
+    if(mode === "dark") {
+        console.log("dark")
+        document.getElementById("dark").style.display = "none";
+        document.getElementById("light").style.display = "inline-block";
+        document.body.classList.add("dark-mode"); // Add dark-mode class to body
+    } else {
+        console.log("light")
+        document.getElementById("light").style.display = "none";
+        document.getElementById("dark").style.display = "inline-block";
+        document.body.classList.remove("dark-mode"); // Add dark-mode class to body 
+    }
+}
